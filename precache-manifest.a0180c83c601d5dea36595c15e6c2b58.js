e09be609a67363b82a3724b5c6c1c380a28223c5
Gaurav Goyal <gauravgoyal129@gmail.com>
2019-04-23 13:49:38 +0530

    Updates

---
+++ precache-manifest.a0180c83c601d5dea36595c15e6c2b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745adc6d7f77a57a03a8f07606741d1e",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "5ca9bca7436a285756a7",
+    "url": "/my-react-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "4a4019e181e24a9f5153",
+    "url": "/my-react-app/static/js/2.309cd016.chunk.js"
+  },
+  {
+    "revision": "5ca9bca7436a285756a7",
+    "url": "/my-react-app/static/js/main.1d9747dd.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
